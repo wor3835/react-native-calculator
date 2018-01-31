d1342a2516aae7a06a82b585f7654910eb081aac
wor3835 <wor3835@rit.edu>
2018-01-31 12:54:37 -0500

    Added more snapshot tests
    Added tests for creating store and loading the operation function.

--- App.test.js
+++ App.test.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import connect from './Main';
+import React from "react";
+import renderer from "react-test-renderer";
+import connect from "./Main";
+import store from "./App";
+import performOp from "./operations";
 
-const app = {
-};
+const app = {};
 
-test('App renders appropriately', () => {
-  const tree = renderer.create(
-    <connect />
-  ).toJSON();
+test("App renders appropriately", () => {
+  const tree = renderer.create(<connect />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("Creates store", () => {
+  const tree = renderer.create(<store />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("Opperation loads correctly", () => {
+  const tree = renderer.create(<performOp />).toJSON();
   expect(tree).toMatchSnapshot();
 });
